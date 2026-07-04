7fc0c3c1dd6f20b6b44d0245f05afa6a6b651e9a
Lavnish Saini <lavnishsaini51@gmial.com>
2026-07-04 11:40:30 +0530

    utils generateToken added

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+
+// Used after successful login/signup to issue an auth token
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE || '7d',
+  });
+};
+
+module.exports = generateToken;
